59101f0f291b484f5de0f991eec39aaabecdd6f0
Maxwell <130581484+Maxw311nyimbili@users.noreply.github.com>
2023-12-09 23:49:24 +0000

    Enable to also send message to outlook, Add prompt to send notification to user's gmail/outlook

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import base64
-
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+
 import os
 import send
 
@@ -10,22 +9,32 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email import encoders
 
+# outlook stuff
+import pythoncom
+
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static\\files'
 
 
 @app.route("/", methods=["POST", "GET"])
 def form():
+
     if request.method == "GET":
         title = "Thank you"
         return render_template("index.html", title=title)
     elif request.method == "POST":
+
         name = request.form.get("name")
-        email = request.form.get("email")
+        user_email = request.form.get("email")
         subject = request.form.get("subject")
         file = request.files['file']
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
-                               secure_filename(file.filename)))
+        try:
+            file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                                   secure_filename(file.filename)))
+        except FileNotFoundError:
+            error_message = "Please upload a file"
+            return render_template("index.html", error=error_message)
 
         filename = f"{os.path.abspath(os.path.dirname(__file__))}\\{app.config['UPLOAD_FOLDER']}\\{secure_filename(file.filename)}"
 
@@ -42,12 +51,17 @@ def form():
         response = ""
         if request.form.get("yes") == "on":
             date = request.form.get("date")
-            response = response + "Student wants tutorial session and would like to meet on: " \
-                       + "\n" + date.split('T')[0] + " at " + date.split('T')[1] + "via a Zoom call."
+            try:
+                response = response + "Student wants tutorial a session and would like to have a Zoom call on " \
+                           + "\n" + date.split('T')[0] + " at " + date.split('T')[1] + "."
+            except IndexError:
+                error_message_1 = "Please enter date and time."
+                return render_template("index.html", error_1=error_message_1)
+
         else:
             response = response + "Student does not want tutorial session"
 
-        body = f"Student name: {name} \nStudent's address: {email} " \
+        body = f"Student name: {name} \nStudent's address: {user_email} " \
                f"\nSubject: {subject}\n\nNB: {response}"
 
         msg = MIMEMultipart()
@@ -56,9 +70,11 @@ def form():
         msg.attach(attachment_package)
         text = msg.as_string()
 
-        send.send_email(text)
+        send.send_email(user_email, filename, body, text)
         message_1 = "Submission was successful!"
-        message2 = "The solutions will be sent to your email in space of four days"
+        message2 = "The solutions will be sent to your email in four days"
+        to_user_email = message_1 + "\n" + message2
+        send.user_send_email(user_email, to_user_email)
         return render_template("index.html", name=message_1, name_1=message2)
 
 
--- send.py
+++ send.py
@@ -1,16 +1,98 @@
 import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# --------------------------
+# outlook stuff
+import win32com.client as win32
+import pythoncom
+# -----------------------
 
-# sending the news to a specified email address
-def send_email(message):
-    host = "smtp.gmail.com"
-    port = 465
+# if you get this error: (-2147221008, 'CoInitialize has not been called.', None, None)
+# close pycharm, run as an administrator, then in the terminal uninstall then install pywin32
+# using pip uninstall pywin32 pypiwin32 to uninstall and pip install pywin32 pypiwin32 to install
 
-    username = "nyimbilimaxwell9@gmail.com"
-    password = "ltrpqvpfwrlsrcnu"
+# SENDING EMAIL TO THROUGH GMAIL
+def send_email(username_placeholder, file, body, message):
+    if username_placeholder.split("@")[1] == "gmail.com":
+        host = "smtp.gmail.com"
+        port = 465
 
-    receiver = "nyimbilimaxwell9@gmail.com"
-    context = ssl.create_default_context()
+        username = "nyimbilimaxwell9@gmail.com"
+        password = "ltrpqvpfwrlsrcnu"
 
-    with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
-        server.sendmail(username, receiver, message)
+        receiver = "nyimbilimaxwell9@gmail.com"
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+    else:
+        send_to_outlook(username_placeholder, file, body)
+
+def user_send_email(useremail, message):
+    if useremail.split("@")[1] == "gmail.com":
+        host = "smtp.gmail.com"
+        port = 465
+
+        msg = MIMEMultipart()
+        msg["Subject"] = "Submission was successful"
+        msg.attach(MIMEText(message, 'plain'))
+        text = msg.as_string()
+
+        username = "nyimbilimaxwell9@gmail.com"
+        password = "ltrpqvpfwrlsrcnu"
+
+        receiver = useremail
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, text)
+    else:
+        user_send_to_outlook(useremail, message)
+# SENDING EMAIL THROUGH OUTLOOK
+# construction of outlook application instance--------------------
+def send_to_outlook(user_name, file, body):
+    try:
+        pythoncom.CoInitialize()
+
+        olApp = win32.Dispatch('Outlook.Application')
+        olNS = olApp.GetNameSpace('MAPI')
+
+        # construct the email item object
+        mailItem = olApp.CreateItem(0)
+        mailItem.Subject = "Submission was successful"
+        mailItem.BodyFormat = 1  # Plain text
+        mailItem.Body = body
+        mailItem.To = user_name
+        mailItem.Attachments.Add(file)
+        mailItem._oleobj_.Invoke(*(64209, 0, 8, 0, olNS.Accounts.Item("maxwell.nyimbili@ashesi.edu.gh")))
+
+        # Send the email
+        mailItem.Send()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def user_send_to_outlook(user_name, message):
+    try:
+        pythoncom.CoInitialize()
+
+        olApp = win32.Dispatch('Outlook.Application')
+        olNS = olApp.GetNameSpace('MAPI')
+
+        # construct the email item object
+        mailItem = olApp.CreateItem(0)
+        mailItem.Subject = "Notification from LoGo Study"
+        mailItem.BodyFormat = 1  # Plain text
+        mailItem.Body = message
+        mailItem.To = user_name
+        mailItem._oleobj_.Invoke(*(64209, 0, 8, 0, olNS.Accounts.Item("maxwell.nyimbili@ashesi.edu.gh")))
+
+        # Send the email
+        mailItem.Send()
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+# # ----------------------------------
+
